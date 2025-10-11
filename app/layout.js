2929d80d3a423d97dd0f3d924f8531d328a68677
matthewadeboyejr <matthewconsult@gmail.com>
2025-10-11 14:56:20 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -37,10 +37,34 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-R6ESZ2TYTN');
           `}
         </Script>
+
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1521308238868785');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable}  antialiased   px-4 sm:px-6 md:px-12 lg:px-24 py-4 sm:py-6  `}
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1521308238868785&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <NavBar />
         <Progress />
         {children}
